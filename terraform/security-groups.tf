aa23228f654b828365a83ab0c42b199cda473e5c
Adonai S. Canez <adonaicanez@gmail.com>
2020-05-24 19:26:14 -0300

    alterações no projeto

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_port_22" {
   provider    = aws.virginia
   name        = "allow_port_22"
-  description = "Allow SSH inbound traffic"
+  description = "Libera entrada na porta 22"
   vpc_id      = "vpc-ef91b595"
 
   ingress {
@@ -26,7 +26,7 @@ resource "aws_security_group" "allow_port_22" {
 resource "aws_security_group" "allow_port_80" {
   provider    = aws.virginia
   name        = "allow_port_80"
-  description = "Allow HTTP inbound traffic"
+  description = "Libera entrada na porta 80"
   vpc_id      = "vpc-ef91b595"
 
   ingress {
@@ -50,8 +50,8 @@ resource "aws_security_group" "allow_port_80" {
 
 resource "aws_security_group" "allow_port_10050" {
   provider    = aws.virginia
-  name        = "allow_10050"
-  description = "Allow Zabbix Server inbound traffic"
+  name        = "allow_port_10050"
+  description = "Libera entrada na porta 10050"
   vpc_id      = "vpc-ef91b595"
 
   ingress {
